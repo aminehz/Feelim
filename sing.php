564fcecd59c0dccbd2736394e3145f5641a3c41d
aminehz <amine.hz.hz.98@gmail.com>
2021-12-20 11:19:51 +0100

    my first commit

---
+++ sing.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$servername="localhost";
+$username="root";
+$password="";
+$data="Feelim";
+$serverport="3308";
+$conn=new pdo($servername,$username,$password,$data,$serverport);
+$sql='SELECT * FROM film';
+$list_F=$conn->prepare($sql);
+$L=$list_F->FetchAll;
+if(!isset($_POST['username'])||!isset($_POST['userpassword']|| empty($_POST['username'])))
+{
+header("location:signing.html");
+} elseif($_POST['username']=$POST)
+
+?>
