678009681cff21540faba8cb0f015c128ae30116
aminehz <amine.hz.hz.98@gmail.com>
2021-12-22 21:23:18 +0100

    my first commit

--- sing.php
+++ sing.php
@@ -3,18 +3,27 @@ session_start();
 $servername="localhost";
 $username="root";
 $password="";
-$data="Feelim";
+$data="feelim";
 $serverport="3308";
-$conn=new pdo($servername,$username,$password,$data,$serverport);
-$sql='SELECT * FROM film';
-$list_F=$conn->prepare($sql);
-$L=$list_F->FetchAll;
-if(!isset($_POST['username'])||!isset($_POST['userpassword']|| empty($_POST['username'])))
+$conn=new mysqli($servername,$username,$password,$data);
+if($conn->connect_error)
+{
+    echo "failed to connect to mysql :" .$conn->connect_error;
+}else 
+{ //echo "Connection succeeded";
+$sql="INSERT INTO user (id,user_id,username,password,email,user_tag,tag_id) values(,,'amine','amine123','em@poly',,)";
+$result=$conn->query($sql);
+/*$row=$result->fetch_assoc();
+printf("%s\n %s",$row["question"],$row["answer_id"]);
+$result->free_result();
+$conn->close();
+}
+/*if(!isset($_POST['username'])||!isset($_POST['userpassword'])|| empty($_POST['username']))
 {
 header("location:signing.html");
-} elseif($_POST['username']=$POST)
-echo "bonjour";
+} //elseif($_POST['username']=$POST)*/
 
+}
 
 
 ?>
